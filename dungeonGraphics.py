ac51a405e38a237d8c8022b7d1600612b1da1e40
jkunimune15 <jkunimune15@punahou.edu>
2016-03-31 20:30:08 -0400

    Made it so the minimap updates automatically, and only shows areas that are explored.

--- dungeonGraphics.py
+++ dungeonGraphics.py
@@ -16,16 +16,10 @@ class DungeonModelView(object):
         self.dispSize = (size[1],size[1])
         tht = 0.3
         self.blockSize = (50,48,15)
-        self.playerPos = (10,10)
+        self.playerPos = (36,36)
         self.screenBounds = (-size[1]/self.blockSize[0]/2, size[1]/self.blockSize[0]/2, -size[1]/self.blockSize[1]/2, size[1]/self.blockSize[1]/2)
         self.bigmap = pygame.Surface((size[0], size[0]))
-
-        self.minimap = pygame.Surface((size[0]-size[1], size[0]-size[1]))
-        for x in range(0,self.minimap.get_width()):
-            for y in range(0,self.minimap.get_height()):
-                self.minimap.set_at((x,y), self.model.getBlock(x*self.model.getWidth()/self.minimap.get_width(),
-                    y*self.model.getHeight()/self.minimap.get_height()).color)
-
+        self.minimap = pygame.Surface((len(dungeon.grid[0]), len(dungeon.grid)))
         self.font = pygame.font.SysFont("Times New Roman", 30, bold=True)
         
 
@@ -34,11 +28,12 @@ class DungeonModelView(object):
             for dx in range(self.screenBounds[0], self.screenBounds[1]+1):
                 block = self.model.getBlock(self.playerPos[0]+dx, self.playerPos[1]+dy)
                 self.bigmap.blit(block.sprite, (dx*self.blockSize[0]+self.dispSize[0]/2, dy*self.blockSize[1]+self.dispSize[1]/2))
+                self.minimap.set_at((self.playerPos[0]+dx, self.playerPos[1]+dy), block.color)
         self.screen.blit(self.bigmap, (0,0))
 
         pygame.draw.rect(self.screen, pygame.Color("black"), (self.size[1], 0, self.size[0]-self.size[1], self.size[1]))    # draw the background of the HUD
 
-        self.screen.blit(self.minimap, (self.size[1],0))    # draw the minimap
+        self.screen.blit(pygame.transform.scale(self.minimap, (size[0]-size[1],size[0]-size[1])), (self.size[1],0))    # draw the minimap
         
         actionLog = self.font.render(self.model.getLog(), 1, (255,255,255,255), (0,0,0,100))    # draw the action log
         self.screen.blit(actionLog, (0, self.size[1]-34))
@@ -57,7 +52,7 @@ if __name__ == '__main__':
     screenY = 720
     size = (screenX, screenY)
     screen = pygame.display.set_mode(size)
-    model = Dungeon(72,72,"panel")
+    model = Dungeon(72,72,"piece")
     
     view = DungeonModelView(model, screen, size)
     #pygame.key.set_repeat(350,35)
