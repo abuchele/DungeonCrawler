5f8f78605334a6d62ddfb1b700ecee325e81ea0d
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-04-19 16:53:39 -0400

    Should encompass all events for the first dungeon

--- eventList.py
+++ eventList.py
@@ -3,7 +3,11 @@
 class checklist():
 	def __init__(self):
 		#universal things
-		world = 0 #Current world completed
+		world = 0 #Current world completed/bosses defeated
+		killcount = 0 #monsters killed
 
 		#World1
 		Tutorial_Dialogue_Finished = False #If they can leave the room.  If not, Mr. E plays id=0003
+		Tutorial_quest = False #Kill 3 monsters
+		Tutorial_quest_confirm = False #Talk to Mr.E after finishing the quest.
+		
