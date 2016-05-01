3872b876af48ca2731093ea34babd60bf72b4253
Maxmilian626 <maxmilian.wei@students.olin.edu>
2016-04-30 20:27:35 -0400

    merge

--- interfaceClasses.py
+++ interfaceClasses.py
@@ -12,7 +12,7 @@ class PyGameKeyboardController(object):
         self.model = model
         self.controls = {pygame.K_e:1,pygame.K_r:1,pygame.K_LEFT:1,pygame.K_RIGHT:1,pygame.K_UP:1,pygame.K_DOWN:1,
         pygame.K_w:1,pygame.K_a:1,pygame.K_s:1,pygame.K_d:1,pygame.K_f:1,pygame.K_TAB:1,pygame.K_LSHIFT:1,
-        pygame.K_1:1,pygame.K_2:1,pygame.K_3:1,pygame.K_4:1,pygame.K_5:1,pygame.K_6:1,pygame.K_7:1,pygame.K_g:1
+        pygame.K_1:1,pygame.K_2:1,pygame.K_3:1,pygame.K_4:1,pygame.K_5:1,pygame.K_6:1,pygame.K_7:1,pygame.K_g:1,
         pygame.K_RETURN:1}
 
         pygame.key.set_repeat()
