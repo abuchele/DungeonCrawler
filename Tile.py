ff066373416c3835f296d761a60d9690650b4e3c
jkunimune15 <jkunimune15@punahou.edu>
2016-05-03 20:22:22 -0400

    Deleting everything

--- Tile.py
+++
@@ -1,9 +0,0 @@
-class Tile(object):
-	def __init__(self):
-		self.color = (0,0,0)
-		self.collides = False
-
-class Floor(Tile):
-	def __init__(self):
-		self.color = (50,50,50)
-		self.collides = False
